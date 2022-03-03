0856ceae3553ef4eb5d05ca8ac507990fb60d8f4
morozovme <m.e.morozov1@gmail.com>
2022-03-03 18:01:36 -0500

    all stars

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(morozovme.com morozovme.com)
+domains=(morozovme.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
